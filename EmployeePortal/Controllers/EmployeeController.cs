2ec51cab6ec220a388a7fb25cf31ca0139efee7e
codeplexindia <alok.kumar.sahoo@hotmail.com>
2025-01-04 19:15:10 +0530

    Add project files.

---
+++ EmployeePortal/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeePortal.Model;
+using EmployeePortal.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository _employeeRepository;
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+                this._employeeRepository = employeeRepository;
+        }
+
+        public async Task<ActionResult> GetEmployees() 
+        {
+            var employees = await _employeeRepository.GetEmployeesAsync();
+            return Ok(employees);
+        }ccccccccccccccccccccccccccccccccccc
+
+        public async Task<ActionResult> AddEmployee(Employee employee)
+        {
+            await _employeeRepository.SaveEmployee(employee);
+            return Ok(employee);
+        }
+    }
+}
---
+++ EmployeePortal/Repository/EmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using EmployeePortal.Data;
+using EmployeePortal.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Repository
+{
+    public class EmployeeRepository
+    {
+        private readonly AppDbContext db;
+
+        public EmployeeRepository(AppDbContext db) 
+        { 
+            this.db = db;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            return await db.Employees.ToListAsync();
+        }
+
+        public async Task SaveEmployee(Employee employee)
+        {
+            await db.Employees.AddAsync(employee);
+            await db.SaveChangesAsync();
+        }
+    }
+}
